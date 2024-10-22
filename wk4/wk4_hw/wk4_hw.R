ebae27472fb3fc2e36252378814c61f2e7a18d75
ceryse <cerysedwards7@gmail.com>
2024-10-22 13:44:15 +0100

    code

--- wk4/wk4_hw/wk4_hw.R
+++ wk4/wk4_hw/wk4_hw.R
@@ -2,12 +2,15 @@ library(tidyverse)
 library(sf)
 library(janitor)
 
+# read gender inequality index data
 gii_data <- read_csv("gii_data.csv") %>%
   clean_names()
 
+# read iso key
 iso <- read_csv("iso.csv") %>%
   clean_names()
 
+# read world countries geojson and merge with gender inequality index data
 world_countries <- st_read("world_countries.geojson") %>%
   clean_names() %>%
   merge(., iso, by.x = "iso", by.y = "alpha_2_code") %>%
